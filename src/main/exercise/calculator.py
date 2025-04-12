516e32c62ef02fddd1c73e18fa838c8871fbd2ea
Ram11Coder <rambeliver11@gmail.com>
2025-04-13 02:51:51 +0530

    Added 2D list

--- src/main/exercise/calculator.py
+++ src/main/exercise/calculator.py
@@ -14,3 +14,17 @@ elif operator == "/":
     print(round(num1 / num2, 2))
 else:
     print(f"Enter the operator {operator} is not valid")
+
+# Calculator using 2D
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*",0,"#"))
+
+
+for num in num_pad:
+    for n in num:
+        print(n, end=" ")
+    print()
+
--- src/main/main.py
+++ src/main/main.py
@@ -260,3 +260,18 @@ days = ("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUN
 print(days)
 print(days.count("MONDAY"))
 print(days.index("WEDNESDAY"))
+
+# 2 Dimension(Matrix)
+
+fruits = ["Apple", "Orange", "Grape"]
+vegetables = ["Carrot", "Brinjal", "Radish"]
+meats = ["Chicken", "Lamb", "Fish"]
+
+groceries = [fruits, vegetables, meats]
+print(groceries)
+print(groceries[1][1])
+
+for grocery in groceries:
+    for food in grocery:
+        print(food, end=" ")
+    print()
