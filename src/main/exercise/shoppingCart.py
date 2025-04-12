726f50f44850ab9326e99bb77cd456a93949a9f1
Ram11Coder <rambeliver11@gmail.com>
2025-04-13 00:45:55 +0530

    Updated shoppingCart program

--- src/main/exercise/shoppingCart.py
+++ src/main/exercise/shoppingCart.py
@@ -1,6 +1,30 @@
 # Excercise : Shopping cart program
 
-food = input("What would you like to order? : ")
-price = float(input("What is the price of food?:"))
-quantity = int(input("What is the quantity of order?:"))
-print(f"Total bill of ordered {food}/s : ${price * quantity}")
+# food = input("What would you like to order? : ")
+# price = float(input("What is the price of food?:"))
+# quantity = int(input("What is the quantity of order?:"))
+# print(f"Total bill of ordered {food}/s : ${price * quantity}")
+
+# Using loop and collection
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter the food (Q/q to quit) : ")
+    if food.lower() == 'q':
+        break;
+    else:
+        price = float(input(f"Enter the price of a {food} : $"))
+        foods.append(food)
+        prices.append(price)
+
+for price in prices:
+    total += price
+
+print("----- YOUR CART -----")
+for food in foods:
+    print(food, end=" ")
+print()
+print(f"Total price of foods = ${total}")
