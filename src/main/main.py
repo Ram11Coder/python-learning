31852ab8f7eee976595eb4e5b26c34a21b631278
Ram11Coder <rambeliver11@gmail.com>
2025-04-14 15:25:10 +0530

    random concepts

--- src/main/main.py
+++ src/main/main.py
@@ -317,3 +317,22 @@ print(items)
 
 for k,v in states_capital.items():
     print(f"{k} : {v}")
+
+
+# Random
+
+import random
+
+low = 1
+high = 100
+print(random.randint(low,high))
+options = ["rock", "paper", "scissor"]
+
+option = random.choice(options)
+print(option)
+
+print(random.random())
+
+cards = ["2", "3", "4","5","6","7","8","9","A","K","Q"]
+random.shuffle(cards)
+print(cards)
