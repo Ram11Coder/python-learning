81a163ac3610aea6ccf44c5ebd696cb2d1471a2a
Ram11Coder <rambeliver11@gmail.com>
2025-04-17 12:51:28 +0530

    var scopes

--- src/main/main.py
+++ src/main/main.py
@@ -523,3 +523,34 @@ print(pi)
 import math_module as mm
 print(mm.pi)
 print(mm.sq(10))
+
+# variable scope : Where a variable is visible and accessible
+# scope resolution : (LEGB) Local -> Enclosed -> Global -> Built-in
+
+# local
+def func():
+    x =1
+    print(x)
+
+func()
+
+# enclosed
+def func1():
+    x =2
+    def func2():
+        print(x)
+    func2()
+
+func1()
+
+# Global - outside of any func()
+x =3
+
+def func3():
+    print(x)
+
+func3()
+
+from math import e
+print(e)
+
