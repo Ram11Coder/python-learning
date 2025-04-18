a28205070397aa481d3803d692116790c74eb979
Ram11Coder <rambeliver11@gmail.com>
2025-04-18 18:52:51 +0530

    Inheritance Types

--- src/main/main.py
+++ src/main/main.py
@@ -666,3 +666,58 @@ dog.sleep()
 dog.speak()
 cat.speak()
 mouse.speak()
+
+
+# Multiple inheritance = Inherit from more than one parent class
+# 						Ex: C(A,B)
+# Multilevel Inheritancce = Inherit from the parent which inherit from another parent
+# 						Ex: C(B) <- B(A) <- A
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+
+class Rabbit(Prey):
+    pass
+
+
+class Hawk(Predator):
+    pass
+
+
+class Fish(Prey, Predator):
+    pass
+
+
+rabbit = Rabbit("Tony")
+rabbit.flee()
+rabbit.eat()
+rabbit.sleep()
+
+hawk = Hawk("Hawky")
+hawk.eat()
+hawk.sleep()
+hawk.hunt()
+
+fish = Fish("Nemo")
+fish.eat()
+fish.sleep()
+fish.flee()
+fish.hunt()
