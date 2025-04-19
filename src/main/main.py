23c4c0b565281e25b0b69f07577cd56fd4908be6
Ram11Coder <rambeliver11@gmail.com>
2025-04-19 12:01:43 +0530

    Added Decorator

--- src/main/main.py
+++ src/main/main.py
@@ -984,7 +984,7 @@ class Book:
         elif item == 'author':
             return self.author
         elif item == 'num_pages':
-            return  self.num_pages
+            return self.num_pages
         else:
             return f"Key {item} was not found"
 
@@ -1000,3 +1000,57 @@ print(book1 < book3)
 print(book1 + book3)
 print("Lion" in book3)
 print(book1['title'])
+
+
+# @Decorator = Decorator used to define a method as a property (It can be accessed like an attribute)
+# Benefits : Add additional logic when read,write or delete attribute
+# Gives you getter, setter and deleter method
+
+# _attribure -> private var
+class Rectangle:
+    def __init__(self, height, width):
+        self._height = height
+        self._width = width
+
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
+
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
+
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Width must be greater than zero")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater than zero")
+
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height has been deleted")
+
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width has been deleted")
+
+
+rectangle = Rectangle(3, 4)
+
+rectangle.height=5
+rectangle.width=6
+print(rectangle.height)
+print(rectangle.width)
+del rectangle.height
+del rectangle.width
