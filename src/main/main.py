d27757de341211babfe88ee4afa105ce9ed5bd1a
Ram11Coder <rambeliver11@gmail.com>
2025-04-20 10:25:09 +0530

    Added Exception

--- src/main/main.py
+++ src/main/main.py
@@ -1086,3 +1086,19 @@ def get_ice_cream(flavour):
 
 
 get_ice_cream("Vanila")
+
+# exception = An event that interrupt the flow of a program
+# (ZeroDivisionError,TypeError,ValueError)
+# 1. try, 2. expect, 3. finally
+
+try:
+    num = int(input("Enter the number : "))
+    print(1/num)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("input should be numeric value")
+except Exception:
+    print("Something went wrong!")
+finally:
+    print("Clean up activity ")
