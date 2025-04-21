b4c2e1b3d6c86a8b8a32b4c4f5b1ca93cd4ce260
Ram11Coder <rambeliver11@gmail.com>
2025-04-21 16:58:37 +0530

    txt,csv,json file writing

--- src/main/main.py
+++ src/main/main.py
@@ -1120,3 +1120,49 @@ if os.path.exists(file_path):
 else:
     print("The location does not exist")
 
+# File writing (.txt, .json, .csv)
+
+txt_data = "I like pizza!"
+file_path = "output.txt"
+
+# using with -> it automatically file also
+# open parameter return the file object
+with open(file=file_path, mode='w') as file:
+    file.write(txt_data)
+    print(f"txt file {file_path} was created")
+
+# mode (x) - create a file if not exists, if it already exists then throes FileExistsError exception
+# mode (r) - read the file
+# mode (a) - append the file
+# mode (w) - It will override the file
+
+import json
+
+employee = {
+    "name": "Rocky",
+    "age": 27,
+    "Job": "Developer"
+}
+json_file_path = "emp.json"
+try:
+    with open(file=json_file_path, mode='x') as file:
+        json.dump(employee, file, indent=4)
+        print(f"json file {json_file_path} was created")
+except FileExistsError:
+    print("The file already exist")
+
+import csv
+
+employee_list = [["Name", "Age", "Job"],
+                ["Ram", 27, "Senior developer"],
+                ["Vicky", 28, "Senior UI developer"],
+                ["Siva", 30, "Tester"], ]
+csv_file_path = "emp.csv"
+try:
+    with open(file=csv_file_path, mode='w',newline="") as file:
+        writer = csv.writer(file)
+        for row in employee_list:
+            writer.writerow(row)
+        print(f"csv file {csv_file_path} was created")
+except FileExistsError:
+    print("The file already exist")
