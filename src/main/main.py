17e65ed8ffac089881ec48cd39218b67c1f3e367
Ram11Coder <rambeliver11@gmail.com>
2025-04-23 23:57:12 +0530

    Added date time module

--- src/main/main.py
+++ src/main/main.py
@@ -1179,7 +1179,6 @@ except FileNotFoundError:
 except PermissionError:
     print('You do not have read permission for that file')
 
-
 # Json file read
 try:
     with open(json_file_path, 'r') as file:
@@ -1199,3 +1198,26 @@ except FileNotFoundError:
     print("That file was not found")
 except PermissionError:
     print('You do not have read permission for that file')
+
+# datetime modules
+
+import datetime
+
+print(datetime.date.today())
+
+date = datetime.date(2025, 4, 23)
+print(f"date : {date}")
+
+time = datetime.time(10, 4, 23)
+print(f"time : {time}")
+now = datetime.datetime.now()
+print(now)
+modifed_date_time = now.strftime("%H:%M:%S %d-%m-%Y")
+print(modifed_date_time)
+
+target_date_time = datetime.datetime(2030, 1, 1, 1, 1, 1)
+
+if target_date_time < now:
+    print("Target date has passed")
+else:
+    print("Target date has not passed")
