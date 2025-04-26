50aa017039d72b60f23b0064ca8f89942f626331
Ram11Coder <rambeliver11@gmail.com>
2025-04-26 08:49:35 +0530

    Making API call in python

--- src/main/main.py
+++ src/main/main.py
@@ -1257,3 +1257,26 @@ chore1.join()
 chore2.join()
 chore3.join()
 print("All chores are completed!")
+
+
+# Fetch API response
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def pokemon_info(name):
+    url = f"{base_url}pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+pokeman = "pikachu"
+
+pokeman_info = pokemon_info(pokeman)
+if pokeman_info:
+    print(pokeman_info["name"].capitalize())
+    print(pokeman_info["id"])
